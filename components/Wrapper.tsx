d9604aaac04b139b59a5dbece88faa9a8e75aab9
Rozstone <wangsiteng123@gmail.com>
2022-12-12 21:29:32 +0800

    增加返回主页

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export const Wrapper = ({ children }: { children: ReactNode }) => {
-	return <div className="p-6 bg-slate-50">{children}</div>;
+	return (
+		<div className="p-6 bg-slate-50">
+			<Link href="/">返回主页</Link>
+			{children}
+		</div>
+	);
 };
